42c93d3a6aafb8b0701fe92dfd91eb4f39cb7782
varaprasad431 <147475432+varaprasad431@users.noreply.github.com>
2024-10-18 14:18:19 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,5 +1,5 @@
-resource "aws_elb" "prasadten" {
-  name               = "prasaddigitalvibeselb"
+resource "aws_elb" "loadbalancer" {
+  name               = "prasadigtalvibes-terraform-elb"
   availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
 
   listener {
@@ -17,26 +17,25 @@ resource "aws_elb" "prasadten" {
     interval            = 30
   }
 
-  instances                 = ["${aws_instance.prasadthree.id}", "${aws_instance.prasadsix.id}"]
+  instances                 = ["${aws_instance.one.id}", "${aws_instance.two.id}"]
   cross_zone_load_balancing = true
   idle_timeout              = 400
   tags = {
-    Name = "raham-tf-elb"
+    Name = "prasadigtalvibes-tf-elb"
   }
 }
-/*
-  resource "aws_rds_cluster" "default" {
+
+ /*resource "aws_rds_cluster" "default" {
   cluster_identifier      = "aurora-cluster-demo"
   engine                  = "aurora-mysql"
-  engine_version          = "14.5"
+  engine_version          = "5.7.mysql_aurora.2.11.2"
   availability_zones      = ["ap-southeast-1a", "ap-southeast-1b"]
   database_name           = "mydb"
-  master_username         = "prasad"
-  master_password         = "prasaddigitalvibes"
+  master_username         = "prasadigtalvibes"
+  master_password         = "prasadigtalvibes431"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
-}
-*/
+}*/
 
 /*resource "aws_instance" "one" {
   for_each = toset(["one", "two", "three"])
